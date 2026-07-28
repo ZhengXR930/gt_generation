3f009174cf7552d05e5accf90140f000621fbdce
Ubuntu <azureuser@az-cpu-01.4gbbrpdjum4exkeszggs15vhhe.xx.internal.cloudapp.net>
2026-07-28 02:39:38 +0000

    Add generated ARVO GT artifacts

---
+++ gt_results/arvo_48993/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ASSET_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+docker run --rm --platform linux/amd64 -v "${ASSET_DIR}/poc:/tmp/poc:ro" --entrypoint /bin/bash n132/arvo:48993-vul -c '/bin/arvo run'
