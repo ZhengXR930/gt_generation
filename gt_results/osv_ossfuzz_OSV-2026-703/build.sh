889cea884d7518761f602197f99967bcd71fd96f
ZhengXR930 <xinran.zheng.23@ucl.ac.uk>
2026-08-24 12:02:42 +0800

    Publish lightweight runtime contracts

--- gt_results/osv_ossfuzz_OSV-2026-703/build.sh
+++ gt_results/osv_ossfuzz_OSV-2026-703/build.sh
@@ -20,9 +20,16 @@ if [[ $# -eq 0 ]]; then
   exit 2
 fi
 PROXY_ENV=()
-for _v in http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY; do
-  if [[ -n "${!_v:-}" ]]; then PROXY_ENV+=(-e "${_v}=${!_v}"); fi
+for _v in http_proxy https_proxy no_proxy all_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY ALL_PROXY GT_APT_MIRROR GT_DEBIAN_APT_MIRROR GT_DEBIAN_SECURITY_MIRROR GT_APK_MIRROR GT_PIP_INDEX_URL GT_PIP_TRUSTED_HOST GT_NPM_REGISTRY GT_GOPROXY GT_GOSUMDB GT_RUSTUP_DIST_SERVER GT_RUSTUP_UPDATE_ROOT GT_CARGO_REGISTRY_INDEX GT_NETWORK_BOOTSTRAP_DISABLED; do
+  if [[ -n "${!_v:-}" ]]; then PROXY_ENV+=(-e "${_v}"); fi
 done
 USER_ENV=(--user "$(id -u):$(id -g)")
 if [[ "${GT_BUILD_AS_ROOT:-0}" == "1" ]]; then USER_ENV=(); fi
-exec docker run --rm "${USER_ENV[@]}" -e HOME=/tmp "${PROXY_ENV[@]}" -v "${ASSET_DIR}:/gt" -v "${REPO_ROOT}:/repo:ro" -w /gt/_work/src "${IMAGE}" bash -lc "$*"
+exec docker run --rm "${USER_ENV[@]}" -e HOME=/tmp "${PROXY_ENV[@]}" -v "${ASSET_DIR}:/gt" -v "${REPO_ROOT}:/repo:ro" -w /gt/_work/src "${IMAGE}" bash -lc 'if [[ -f /usr/local/bin/gt-network-bootstrap ]]; then
+  source /usr/local/bin/gt-network-bootstrap
+elif [[ -f /repo/docker/gt-memory-env/network_bootstrap.sh ]]; then
+  source /repo/docker/gt-memory-env/network_bootstrap.sh
+fi
+
+exec bash -lc "$*"
+' build-wrapper "$*"
